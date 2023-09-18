9f9b99660d9a600ae68f30a7a10f44924c0addb9
justin-vanwinkle <vanwinkle.justin@gmail.com>
2023-09-18 15:56:58 -0400

    fix

--- seesaw_pull.py
+++ seesaw_pull.py
@@ -7,18 +7,26 @@ from os.path import exists
 
 headers = {}
 
+
+
 children = [
     {
         "name":"reed",
         "child_id": "person.c3d5a121-230a-4a64-b8eb-637f4fe931b0",
-        "class_id": "class.f0755104-576a-4b92-af87-639a0953a5ba"
+        # "class_id": "class.f0755104-576a-4b92-af87-639a0953a5ba" # Kindergarten - Peterkin
+        "class_id": "class.3d74f8bf-528e-442c-87ad-f8e42e576ef9" # 1st - Johnson
     },
     {
         "name":"neely",
         "child_id": "person.5b4924dd-bd20-4be3-8f7a-2cd5f9b6dc5d",
-        "class_id": "class.878392ae-dfb8-4e1b-8cba-cab0350cffd3"
+        # "class_id": "class.878392ae-dfb8-4e1b-8cba-cab0350cffd3" # Pre-K4 - Maitland
+        "class_id": "class.093990c7-6bd0-475f-8bed-20efdd1dd97d" # Kindergarten - Peterkin
     }
 ]
+
+# No idea what this is
+# "class_id": "class.1919525d-b143-42bd-860a-27101ff5a16d" 
+
 
 
 def download_item(filename, url, create_date, child_name, item_type):
@@ -31,6 +39,7 @@ def download_item(filename, url, create_date, child_name, item_type):
                 os.utime(filepath, (create_date, create_date))
 
 def main(argv):
+    counter = 0
     bearer_token = argv[0]
     headers = {'Authorization': bearer_token}
     
@@ -42,7 +51,9 @@ def main(argv):
 
         items = feed['response']['items']['objects']
         for item in items:
-            time.sleep(1)
+            counter += 1
+            print("processing page " + counter.__str__())
+            time.sleep(.5)
             item_id = item['item']['item_id']
 
             item_url = f"https://app.seesaw.me/api/item_v2?item_id={item_id}"
@@ -51,43 +62,48 @@ def main(argv):
             
             create_date = item_page['response']['item']['create_date']
 
-            
-            if hasattr(item_page['response']['item'], 'pages'):
-                print('processing page')
+            try:
                 pages = item_page['response']['item']['pages']['objects']
-                for page in pages:
-                    time.sleep(.5)
-                    # TODO -- add this to the image
-                    # image_caption = ""
-                    # if "caption" in page:
-                    #     image_caption = page['caption']
+            except:
+                print("pages don't exist here")
+                print(item_id)
+                print(item_url)
+                print(item_page['response']['item'].__dir__())
+                
+            for page in pages:
+                time.sleep(.3)
+                # TODO -- add this to the image
+                # image_caption = ""
+                # if "caption" in page:
+                #     image_caption = page['caption']
 
-                    # if image
-                    if len(page['composite_image_map']['compositeImageMap']) == 0:
-                        try:
-                            filename = f"{page['item_page_id']}.jpg"
-                            item_url = page['composite_image_url']
-                            download_item(filename, item_url, create_date, child["name"], item_type="image",)
-                        except:
-                            print("failed on image")
-                            print(page)
+                # if image
+                if len(page['composite_image_map']['compositeImageMap']) == 0:
+                    try:
+                        filename = f"{page['item_page_id']}.jpg"
+                        item_url = page['composite_image_url']
+                        download_item(filename, item_url, create_date, child["name"], item_type="image",)
+                    except:
+                        print("failed on image")
+                        print(page)
+
+                else:
+                    if (page['composite_image_map']['compositeImageMap'][0]['actions'][0]['action'] == "link"):
+                        print(f"Title: {page['composite_image_map']['compositeImageMap'][0]['actions'][0]['payload']['externalLinkDetails']['title']}")
+                        print(f"Link: {page['composite_image_map']['compositeImageMap'][0]['actions'][0]['payload']['destination']}")
+                        print(f"Caption: {page['caption']}")
+                        print("----------")
 
                     else:
-                        if (page['composite_image_map']['compositeImageMap'][0]['actions'][0]['action'] == "link"):
-                            print(f"Title: {page['composite_image_map']['compositeImageMap'][0]['actions'][0]['payload']['externalLinkDetails']['title']}")
-                            print(f"Link: {page['composite_image_map']['compositeImageMap'][0]['actions'][0]['payload']['destination']}")
-                            print(f"Caption: {page['caption']}")
-                            print("----------")
-
-                        else:
+                        try:
                             # this is a video
                             filename = f"{page['item_page_id']}.mov"
                             item_url = page['composite_image_map']['compositeImageMap'][0]['actions'][0]['payload']['videoSrc']
                             download_item(filename, item_url, create_date, child["name"], item_type="video")
-               
-            else:
-                # print(item_page['response']['item'].__dir__())
-                print(item_page['response']['item']['items'])
+                        except:
+                            print("failed on video")
+                            print(page)
+            
                 
 if __name__ == "__main__":
     main(sys.argv[1:])
